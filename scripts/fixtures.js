85eb242bf678fb64fc233cde6968167602d10a18
Alberto <alberto.grau.margarida@gmail.com>
2016-10-05 19:05:51 -0400

    WIP: moved album objects to fixtures.js

---
+++ scripts/fixtures.js
@@ -0,0 +1,48 @@
+// We place all (data) album objects in this file.
+//ALBUM 1
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        { title: 'Blue', duration: '4:26' },
+        { title: 'Green', duration: '3:14' },
+        { title: 'Red', duration: '5:01' },
+        { title: 'Pink', duration: '3:21' },
+        { title: 'Magenta', duration: '2:15' }
+    ]
+};
+
+//ALBUM 2
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        { title: 'Hello, Operator?', duration: '1:01' },
+        { title: 'Ring, ring, ring', duration: '5:01' },
+        { title: 'Fits in your pocket', duration: '3:21' },
+        { title: 'Can you hear me now?', duration: '3:14' },
+        { title: 'Wrong phone number', duration: '2:15' }
+    ]
+};
+
+//ALBUM 3
+var albumEmoji = {
+  title: 'Songs of Emoji',
+  artist: 'Dr Moji',
+  label: 'Text Records',
+  year: '2016',
+  albumArtUrl: 'assets/images/album_covers/02.png',
+  songs: [
+      { title: 'Smiley', duration: '1:01' },
+      { title: 'Dancing Lady in Red', duration: '5:01' },
+      { title: 'Cool Sunglasses', duration: '2:54' },
+      { title: 'Taco Emoji', duration: '3:49' },
+      { title: 'Poop Emoji', duration: '0:12' }
+    ]
+};
